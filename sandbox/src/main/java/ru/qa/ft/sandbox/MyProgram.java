0a79ce0ae6df7b2f72abbca2d3aed2fd93b4072f
YelenaKharina <elena.xarina2014@gmail.com>
2020-03-11 21:51:28 +0300

    add some function (les 1.7)

--- sandbox/src/main/java/ru/qa/ft/sandbox/MyProgram.java
+++ sandbox/src/main/java/ru/qa/ft/sandbox/MyProgram.java
@@ -3,7 +3,27 @@ package ru.qa.ft.sandbox;
 public class MyProgram {
 
 	public static void main (String[] arg) {
-		System.out.println("Hello,world!");
+		hello("world");
+		hello("user");
+		hello("Elena");
+
+		double l = 3;
+		System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+		double a = 4;
+		double b = 5;
+		System.out.println("Площадь прямоугольника со сторонами " + a + " и " + b + " = " + area(a, b));
 	}
 
+	public static void hello (String object) {
+		System.out.println("Hello, " + object + "!");
+	}
+
+	public static double area (double l) {
+		return l * l;
+	}
+
+	public static double area (double a, double b) {
+		return a * b;
+	}
 }
