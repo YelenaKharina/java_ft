8142090d001a48b62e75b1a03eee5a9162bd6902
YelenaKharina <elena.xarina2014@gmail.com>
2020-03-23 22:59:56 +0300

    refactor (task#4)
    task#4

---
+++ addressbook-web-tests/src/test/java/ru/qa/ft/addressbook/ContactData.java
@@ -0,0 +1,43 @@
+package ru.qa.ft.addressbook;
+
+public class ContactData {
+  private final String firstname;
+  private final String middlename;
+  private final String lastname;
+  private final String address2;
+  private final String email;
+  private final String home;
+
+  public ContactData(String firstname, String middlename, String lastname, String address2, String email, String home) {
+    this.firstname = firstname;
+    this.middlename = middlename;
+    this.lastname = lastname;
+    this.address2 = address2;
+    this.email = email;
+    this.home = home;
+  }
+
+  public String getFirstname() {
+    return firstname;
+  }
+
+  public String getMiddlename() {
+    return middlename;
+  }
+
+  public String getLastname() {
+    return lastname;
+  }
+
+  public String getAddress2() {
+    return address2;
+  }
+
+  public String getEmail() {
+    return email;
+  }
+
+  public String getHome() {
+    return home;
+  }
+}
