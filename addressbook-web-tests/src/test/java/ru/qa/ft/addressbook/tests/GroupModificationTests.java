1776142a2cfb5017a3e5790f5f3d088d8618ac70
YelenaKharina <elena.xarina2014@gmail.com>
2020-03-25 21:23:50 +0300

    create GroupModificationTests

---
+++ addressbook-web-tests/src/test/java/ru/qa/ft/addressbook/tests/GroupModificationTests.java
@@ -0,0 +1,17 @@
+package ru.qa.ft.addressbook.tests;
+
+import org.testng.annotations.Test;
+import ru.qa.ft.addressbook.module.GroupData;
+
+public class GroupModificationTests extends TestBase {
+
+  @Test
+  public void testGroupModification() {
+    app.getNavigationHelper().goToGroupPage();
+    app.getNavigationHelper().selectElement();
+    app.getGroupHelper().initGroupModification();
+    app.getGroupHelper().fillGroupForm(new GroupData("name1", "header2", "footer3"));
+    app.getGroupHelper().submitGroupModification();
+    app.getGroupHelper().returnToGroupPage();
+  }
+}
