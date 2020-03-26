930511366081931dc100b8aa1bdc033c6b2dac74
YelenaKharina <elena.xarina2014@gmail.com>
2020-03-26 16:25:19 +0300

    исправлены замечания. теперь используем BrowserType из пакета селениум

--- addressbook-web-tests/src/test/java/ru/qa/ft/addressbook/appmanager/ApplicationManager.java
+++ addressbook-web-tests/src/test/java/ru/qa/ft/addressbook/appmanager/ApplicationManager.java
@@ -3,8 +3,7 @@ package ru.qa.ft.addressbook.appmanager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import sun.plugin2.util.BrowserType;
-
+import org.openqa.selenium.remote.BrowserType;
 import java.util.concurrent.TimeUnit;
 
 public class ApplicationManager {
@@ -14,17 +13,17 @@ public class ApplicationManager {
   private SessionHelper sessionHelper;
   private NavigationHelper navigationHelper;
   private GroupHelper groupHelper;
-  private int browser;
+  private String browser;
 
-  public ApplicationManager(int browser) {
+  public ApplicationManager(String browser) {
     this.browser = browser;
   }
 
 
   public void init() {
-    if (browser == BrowserType.MOZILLA) {
+    if (browser.equals(BrowserType.FIREFOX)) {
       wd = new FirefoxDriver();
-    } else  if (browser == BrowserType.DEFAULT) {
+    } else  if (browser.equals(BrowserType.CHROME)) {
       wd = new ChromeDriver();
     }
 
